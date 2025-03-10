1de126ee208f44d9a940517025c7d2f02dce7f0b
webdmitriev <webdmitriev@gmail.com>
2025-03-10 16:31:38 +0300

    Added TitleForBlocksCell

---
+++ construction-webdmitriev/Sections/TitleForBlocks/TitleForBlocks.swift
@@ -0,0 +1,8 @@
+//
+//  titleForBlocks.swift
+//  construction-webdmitriev
+//
+//  Created by Олег Дмитриев on 10.03.2025.
+//
+
+import Foundation
---
+++ construction-webdmitriev/Sections/TitleForBlocks/TitleForBlocksCell.swift
@@ -0,0 +1,8 @@
+//
+//  TitleForBlocksCell.swift
+//  construction-webdmitriev
+//
+//  Created by Олег Дмитриев on 10.03.2025.
+//
+
+import Foundation
